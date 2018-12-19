fa5e6d28095053a47cdf39e34b5d88c2ffad21b5
Kryol <pro.ferigo.theo@gmail.com>
2018-12-19 11:27:24 +0100

    Création du form et de newsletter.php

---
+++ newsletter.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un email par formulaire</title>
+</head>
+
+<body>
+
+    <?php
+
+    foreach ($_POST['invention'] as $inventions) {
+      echo $inventions.', ';
+    }
+
+    $retour = mail($_POST['mail'], '[EMITIME] Nice to meet you, darling',
+    'Salut '.$_POST['prenom'].', on bien reçu votre inscription pour la newsletter des machine : '.$inventions,
+     'From : contact@emitime.com');
+
+    if ($retour) {
+        echo '<p>Votre message a bien été envoyé !</p>';
+    }
+
+    ?>
+</body>
+
+</html>
